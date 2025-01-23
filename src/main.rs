8b128e0f3935735b87b4ac07dc212109d905f5c5
Paul van der Meijs <vandermeijs@redkiwi.nl>
2025-01-23 16:27:27 +0100

    Add `alias` command (closes #2)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Create files and folders from templates
 #[derive(Parser, Debug)]
 #[command(name = "Spawn", version, about, long_about = None)]
 struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
     /// Path to a template
     #[arg()]
     path: Option<String>,
 }
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Add an alias for a URL
+    Alias { name: String, url: String },
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    let result: Result<usize, String> = match cli.path {
-        Some(_) => todo!("Implement `path`"),
-        None => Err("Provide a path".to_string()),
+    let result: Result<usize, String> = match (cli.path, cli.command) {
+        (Some(_), None) => todo!("Implement `path`"),
+        (None, Some(Commands::Alias { name: _, url: _ })) => todo!("Implement `alias`"),
+        _ => Err("Provide either a command or path".to_string()),
     };
 
     let code = match result {
